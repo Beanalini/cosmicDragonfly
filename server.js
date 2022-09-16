365cb3e6b1803f7b713242047d737fe6d5360c1a
Beanalini <dat826@gmail.com>
2022-09-16 21:28:25 +0100

    added code to file

---
+++ server.js
@@ -0,0 +1,39 @@
+
+//add dependancies
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index.js');
+const fs = require('fs');
+//const util = require('util');
+
+
+//create instanace of express app
+const PORT = 3000;
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+//create route to public assets folder
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// GET Route for wildcard page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
